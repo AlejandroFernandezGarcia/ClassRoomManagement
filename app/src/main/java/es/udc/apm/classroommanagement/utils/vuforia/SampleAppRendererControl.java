a50f6d96681fa82c54f2c76719b16a33b1a59d82
Alejandro Fernandez Garcia <alejandro.fernandez@udc.es>
2017-05-13 18:52:46 +0200

    Lanza vuforia y reconoce los marcadores

---
+++ app/src/main/java/es/udc/apm/classroommanagement/utils/vuforia/SampleAppRendererControl.java
@@ -0,0 +1,15 @@
+package es.udc.apm.classroommanagement.utils.vuforia;
+
+import com.vuforia.State;
+
+/**
+ * Created by Alejandro on 13/05/2017.
+ */
+
+public interface SampleAppRendererControl {
+
+    // This method has to be implemented by the Renderer class which handles the content rendering
+    // of the sample, this one is called from SampleAppRendering class for each view inside a loop
+    void renderFrame(State state, float[] projectionMatrix);
+
+}
